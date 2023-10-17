83faab348e468d0db576ad0376978f249dbdfbef
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-10-17 15:50:13 +0300

    complete

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class Hous {
+  protected door: boolean;
+  protected tenants: Person[];
+  constructor(protected key: Key) {
+    this.door = false;
+  }
+  comeIn(person: Person): void {
+    if ((this.door = true)) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends Hous {
+  openDoor(key: Key): boolean {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      return this.door;
+    }
+    return false;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
